85f29ba79d511a36a19b32da5b86fe2287b6ab85
Tseegii Tseegii <burtgel99@gmail.com>
2020-11-24 22:40:00 +0800

    last version

--- services/functions/ConvertToVector.py
+++ services/functions/ConvertToVector.py
@@ -1,10 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import math
-
-
-def list_pow(list, pow_num=1):
-    return [number ** pow_num for number in list]
 
 
 class ConvertToVector:
@@ -15,13 +12,13 @@ class ConvertToVector:
     def convert_count_vector(self, data):
         count_vec = CountVectorizer(stop_words=self.s_words)
 
-        count_data = count_vec.fit_transform(data).toarray()
+        count_data = count_vec.fit_transform(data)
 
         # pd.set_option("display.max_rows", None, "display.max_columns", None)
         # cv_dataframe = pd.DataFrame(count_data, columns=count_vec.get_feature_names())
         # print(cv_dataframe)
 
-        return count_data
+        return count_data.toarray()
 
     def convert_tfidf_vector(self, data):
         tfidf_vec = TfidfVectorizer(stop_words=self.s_words)
@@ -33,11 +30,6 @@ class ConvertToVector:
         return tfidf_data
 
     def cosine_similarity(self, user_vector, dic_vector):
-        vector_sum = 0
-
-        vector_sqrt = math.sqrt(sum(list_pow(user_vector, 2))) * math.sqrt(sum(list_pow(dic_vector, 2)))
-        vector_sum = sum(user_vector * dic_vector)
-
-        cosine_number = vector_sum / vector_sqrt
+        cosine_number = cosine_similarity(user_vector.reshape(1, -1), dic_vector.reshape(1, -1))
 
         return cosine_number
