9b8240a7ecb0e527431ca12c54d7ab6d03cb9cee
Tseegii Tseegii <burtgel99@gmail.com>
2020-11-25 07:42:51 +0800

    hello

--- services/views.py
+++ services/views.py
@@ -6,7 +6,7 @@ from services.functions.CosineResult import CosineResult
 from services.functions.ConvertToVector import ConvertToVector
 from django.views.decorators.csrf import csrf_exempt
 import openpyxl
-from services.models import Title, Data
+from services.models import Title, Data, Question
 
 
 def index(request):
@@ -28,15 +28,48 @@ def index(request):
 
 
 @csrf_exempt
+def add_feedback(request):
+    if request.method == 'POST':
+        if request.body:
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            try:
+                token = body['token']
+                question = body['question']
+                id = body['id']
+                if token and question and id:
+                    isHave = Question.objects.filter(content=question).exists()
+                    if not isHave:
+                        Question.objects.create(content=question, knowledge_data_id=id)
+                        response = JsonResponse({'message': 'Амжилттай хадгалагдлаа'})
+                        response.status_code = 201
+                        return response
+                    else:
+                        response = JsonResponse({'message': 'Бүртгэгдсэн байна'})
+                        response.status_code = 200
+                        return response
+                else:
+                    response = JsonResponse({'message': 'Өгөгдөл оруулна уу'})
+                    response.status_code = 400
+                    return response
+            except Exception as e:
+                response = JsonResponse({'message': 'Хүсэлт буруу байна'})
+                response.status_code = 401
+                return response
+    else:
+        return render(request, '405error.html')
+
+
+@csrf_exempt
 def search(request):
     if request.method == 'POST':
         if request.body:
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
             try:
-                body_unicode = request.body.decode('utf-8')
-                body = json.loads(body_unicode)
                 token = body['token']
                 question = body['question']
-                if question:
+                if token and question:
                     stop_word = ['нь', 'энэ', 'бол', 'дэх']
                     user_input = question
 
@@ -49,7 +82,7 @@ def search(request):
                             'id': obj.id,
                             'article': obj.article,
                             'title': Title.objects.get(pk=obj.title_id).title,
-                            'content': obj.content
+                            'content': obj.content,
                         }
                         fullContents.append(data)
                         contents.append(obj.content)
@@ -108,7 +141,6 @@ def excel_import(request):
 
         wb = openpyxl.load_workbook(file)
         worksheet = wb["Sheet1"]
-
         excel_data = list()
         for row in worksheet.iter_rows():
             row_data = list()
@@ -117,13 +149,15 @@ def excel_import(request):
             excel_data.append(row_data)
 
         saved_title = list()
-        last_pk = 0
+        last_title_id = 0
+        last_content_id = 0
         for data in excel_data:
             if len(saved_title) == 0:
+                saved_title.append(data[1])
                 title = Title.objects.create(title=data[1])
-                last_pk = title.id
-                Data.objects.create(article=data[0], title_id=last_pk, content=data[2])
-
+                last_title_id = title.id
+                newData = Data.objects.create(article=data[0], title_id=last_title_id, content=data[2])
+                last_content_id = newData.id
             else:
                 saved = False
                 for title in saved_title:
@@ -132,10 +166,23 @@ def excel_import(request):
                 if saved != True:
                     saved_title.append(data[1])
                     title = Title.objects.create(title=data[1])
-                    last_pk = title.id
-                    Data.objects.create(article=data[0], title_id=last_pk, content=data[2])
+                    last_title_id = title.id
+                    splitted_artice = data[0].split('.')
+                    if len(splitted_artice) > 3:
+                        Data.objects.create(article=data[0], title_id=last_title_id, content=data[2],
+                                            data_id=last_content_id)
+                    else:
+                        newData = Data.objects.create(article=data[0], title_id=last_title_id, content=data[2])
+                        last_content_id = newData.id
+                        print(last_content_id)
                 else:
-                    Data.objects.create(article=data[0], title_id=last_pk, content=data[2])
+                    splitted_artice = data[0].split('.')
+                    if len(splitted_artice) > 3:
+                        Data.objects.create(article=data[0], title_id=last_title_id, content=data[2],
+                                            data_id=last_content_id)
+                    else:
+                        newData = Data.objects.create(article=data[0], title_id=last_title_id, content=data[2])
+                        last_content_id = newData.id
 
         messages.success(request, 'Амжилттай хадгаллаа')
         return render(request, 'fileupload.html')
